2fb29f0faa4bc936f226b72f7d895f3898a6ee7c
Trung Truong <ttruong91498@gmail.com>
2019-08-13 22:47:46 -0400

    Minor fix to test-bitpack driver, update gitignore

--- tests/test_bitpack.c
+++ tests/test_bitpack.c
@@ -7,19 +7,19 @@
 *   Description: Test driver for bitpack module
 *
 ****************************************************************/
-
+#include <stdint.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "../include/bitpack.h"
 
 int main() {
     uint64_t test_1 = 0xFFFFFFFFFFFFFFFF;
-    printf("Bitpack_get_u: %llu \n", Bitpack_getu(test_1, 10, 5));
-
+    printf("Bitpack_get_u: %"PRIu64" \n", Bitpack_getu(test_1, 10, 5));
     test_1 = Bitpack_newu(test_1, 60, 4, 0);
-    printf("Bitpack_new_u: %llu \n", test_1);
+    printf("Bitpack_new_u: %"PRIu64" \n", test_1);
 
-    printf("Bitpack_get_u: %llu \n", Bitpack_getu(test_1, 10, 5));
+    printf("Bitpack_get_u: %"PRIu64" \n", Bitpack_getu(test_1, 10, 5));
 
     return 0;
 }
